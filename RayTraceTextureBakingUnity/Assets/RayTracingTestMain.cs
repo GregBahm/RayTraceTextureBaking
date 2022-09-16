2332012211efa37c2d89a6300e73eeb2bbfbb719
Greg Bahm <grbahm@microsoft.com>
2022-09-16 15:55:29 -0700

    Couldn't get this working without modifying the HDPR source.

--- RayTraceTextureBakingUnity/Assets/RayTracingTestMain.cs
+++ RayTraceTextureBakingUnity/Assets/RayTracingTestMain.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class RayTracingTestMain : MonoBehaviour
 {
@@ -23,34 +26,93 @@ public class RayTracingTestMain : MonoBehaviour
 
     private RayTracingAccelerationStructure accelerationStructure;
 
+    [SerializeField]
+    public Texture2D owenScrambled256Tex;
+    [SerializeField]
+    public Texture2D scramblingTile256SPP;
+    [SerializeField]
+    public Texture2D rankingTile256SPP;
+    [SerializeField]
+    public Texture2D scramblingTex;
+
+    private TextureHandle skyTexture;
+    RenderGraph renderGraph;
+
     void Start()
     {
-        renderTex = new RenderTexture(1024, 1024, 1, RenderTextureFormat.ARGBFloat);
-        renderTex.enableRandomWrite = true;
-        renderTex.Create();
+        List<RenderGraph> graphs = RenderGraph.GetRegisteredRenderGraphs();
+        renderGraph = RenderGraph.GetRegisteredRenderGraphs().First(item => item.name == "HDRP");
+        renderTex = CreateOutputTexture();
+        skyTexture = CreateSkyTexture();
+        accelerationStructure = CreateAccelerationStructure();
+        accelerationStructure.Build();
+    }
 
+    private TextureHandle CreateSkyTexture()
+    {
+        TextureDesc desc = new TextureDesc(Vector2.one, true, true);
+        desc.colorFormat = GraphicsFormat.R32G32B32A32_SFloat;
+        desc.useMipMap = false;
+        desc.autoGenerateMips = false;
+        desc.enableRandomWrite = false;
+        desc.name = "PathTracingSkyBackgroundBuffer";
+        return renderGraph.CreateSharedTexture(desc);
+    }
+
+    private RenderTexture CreateOutputTexture()
+    {
+        RenderTexture ret = new RenderTexture(1024, 1024, 1, RenderTextureFormat.ARGBFloat);
+        ret.enableRandomWrite = true;
+        ret.Create();
+        return ret;
+    }
+
+    private RayTracingAccelerationStructure CreateAccelerationStructure()
+    {
         var settings = new RayTracingAccelerationStructure.RASSettings();
         settings.layerMask = layerMask;
         settings.managementMode = RayTracingAccelerationStructure.ManagementMode.Automatic;
         settings.rayTracingModeMask = RayTracingAccelerationStructure.RayTracingModeMask.Everything;
-
-        accelerationStructure = new RayTracingAccelerationStructure(settings);
-
-        accelerationStructure.Build();
+        return new RayTracingAccelerationStructure(settings);
     }
 
     private void OnDestroy()
     {
+        renderGraph.Cleanup();
         renderTex.Release();
     }
 
     void Update()
     {
+        List<RenderGraph> graphs = RenderGraph.GetRegisteredRenderGraphs();
         outputDisplayMat.SetTexture("_UnlitColorMap", renderTex);
 
-        tracingShader.SetAccelerationStructure("_SceneAccelStruct", accelerationStructure);
-        tracingShader.SetFloat("_Zoom", Mathf.Tan(Mathf.Deg2Rad * Camera.main.fieldOfView * 0.5f));
+        tracingShader.SetAccelerationStructure("_RaytracingAccelerationStructure", accelerationStructure);
+
+        tracingShader.SetTexture("_OwenScrambledTexture", owenScrambled256Tex);
+        tracingShader.SetTexture("_ScramblingTileXSPP", scramblingTile256SPP);
+        tracingShader.SetTexture("_RankingTileXSPP", rankingTile256SPP);
+        tracingShader.SetTexture("_ScramblingTexture", scramblingTex);
+
+        tracingShader.SetInt("_RaytracingCameraSkyEnabled", 0);
+
+        //ConstantBuffer.PushGlobal(ctx.cmd, data.shaderVariablesRaytracingCB, HDShaderIDs._ShaderVariablesRaytracing);
+        //ctx.cmd.SetGlobalBuffer(HDShaderIDs._RaytracingLightCluster, data.lightCluster.GetCluster());
+        //ctx.cmd.SetGlobalBuffer(HDShaderIDs._LightDatasRT, data.lightCluster.GetLightDatas());
+        //ctx.cmd.SetRayTracingIntParam(data.pathTracingShader, HDShaderIDs._RaytracingCameraSkyEnabled, data.cameraData.skyEnabled ? 1 : 0);
+        //ctx.cmd.SetRayTracingVectorParam(data.pathTracingShader, HDShaderIDs._RaytracingCameraClearColor, data.backgroundColor);
+        //ctx.cmd.SetRayTracingTextureParam(data.pathTracingShader, HDShaderIDs._SkyCameraTexture, data.sky);
+        //ctx.cmd.SetRayTracingTextureParam(data.pathTracingShader, HDShaderIDs._SkyTexture, data.skyReflection);
+        tracingShader.SetTexture("_SkyCameraTexture", skyTexture);
+
         tracingShader.SetTexture("_Output", renderTex);
-        tracingShader.Dispatch("MainRayGenShader", renderTex.width, renderTex.height, 1, theCamera);
+        
+        tracingShader.SetAccelerationStructure("_RaytracingAccelerationStructure", accelerationStructure);
+
+        //ctx.cmd.SetRayTracingMatrixParam(data.pathTracingShader, HDShaderIDs._PixelCoordToViewDirWS, data.pixelCoordToViewDirWS);
+        //ctx.cmd.SetRayTracingVectorParam(data.pathTracingShader, HDShaderIDs._PathTracingDoFParameters, data.dofParameters);
+        //ctx.cmd.SetRayTracingVectorParam(data.pathTracingShader, HDShaderIDs._PathTracingTilingParameters, data.tilingParameters);
+
+        tracingShader.Dispatch("RayGen", renderTex.width, renderTex.height, 1, theCamera);
     }
 }
